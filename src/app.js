622c55efa26d51ec4f11216206abc77b06019deb
Sourabh SIddhu <sourabh.siddhu@akridata.com>
2025-05-01 12:17:23 +0530

    minor fix

--- src/app.js
+++ src/app.js
@@ -1,34 +1,25 @@
-const fs = require('fs')
-const path = require('path')
-const express = require('express')
-const app = express()
-const httpolyglot = require('httpolyglot')
-const https = require('https')
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+const app = express();
+const httpolyglot = require("httpolyglot");
+const https = require("https");
 
 //////// CONFIGURATION ///////////
 
 // insert your own ssl certificate and keys
-const options = {
-    key: fs.readFileSync(path.join(__dirname,'..','ssl','key.pem'), 'utf-8'),
-    cert: fs.readFileSync(path.join(__dirname,'..','ssl','cert.pem'), 'utf-8')
-}
+const options = {};
 
-const port = process.env.PORT || 3012
+const port = process.env.PORT || 3012;
 
 ////////////////////////////
 
-require('./routes')(app)
+require("./routes")(app);
 
-const httpsServer = httpolyglot.createServer(options, app)
-const io = require('socket.io')(httpsServer)
-require('./socketController')(io)
-
+const httpsServer = httpolyglot.createServer(options, app);
+const io = require("socket.io")(httpsServer);
+require("./socketController")(io);
 
 httpsServer.listen(port, () => {
-    console.log(`listening on port ${port}`)
-})
-
-
-
-
-
+  console.log(`listening on port ${port}`);
+});
